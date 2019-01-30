82414036c02f6520042840bf408aa04f57582ad5
SowmyaDasari <sowzs26@gmail.com>
2019-01-30 22:11:20 +0530

    Update print.java

--- print.java
+++ print.java
@@ -2,6 +2,7 @@ public class helloworld{
 	public static void main(String[] args){
 		System.out.println("Name : Sowmya Dasari\n");
 		System.out.println("Roll Number : IMT2015507\n");
+		System.out.println("College : IIIT-Bangalore\n");
 		System.exit(0);
 	}
 }
