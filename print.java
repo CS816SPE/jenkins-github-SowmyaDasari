517739d04ec3097d7de31cb0fbff2838c5ab1585
SowmyaDasari <sowzs26@gmail.com>
2019-01-30 22:02:19 +0530

    Add files via upload

---
+++ print.java
@@ -0,0 +1,7 @@
+public class helloworld{
+	public static void main(String[] args){
+		System.out.println("Name : Sowmya Dasari\n");
+		System.out.println("Roll Number : IMT2015507\n");
+		System.exit(0);
+	}
+}
